69596d3e57689e8aad248b872514eb08dd6bef96
venky91 <venksundram@gmail.com>
2013-09-11 18:37:51 -0700

    added unimplemented on click listeners for the two buttons in MainActivity.java

--- src/com/example/extremenotepad/MainActivity.java
+++ src/com/example/extremenotepad/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.extremenotepad;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button addNote;
+	Button viewNotes;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addNote = (Button) findViewById(R.id.addNote);
+		viewNotes = (Button) findViewById(R.id.viewNotes);
+		
+		addNote.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		viewNotes.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 	@Override
