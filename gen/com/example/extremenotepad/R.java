0ab0aee2d2aa0cd6059268b08bdbfc223fb4ee64
venky91 <venksundram@gmail.com>
2013-09-11 18:56:12 -0700

    added add_note activity

--- gen/com/example/extremenotepad/R.java
+++ gen/com/example/extremenotepad/R.java
@@ -36,12 +36,14 @@ public final class R {
         public static final int add_note=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int add_note=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_add_note=0x7f050003;
     }
     public static final class style {
         /** 
