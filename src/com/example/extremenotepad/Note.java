a7dc677906e3dff9537ad5f10b6339c8218aaeed
venky91 <venksundram@gmail.com>
2013-09-11 19:19:13 -0700

    added Note class

---
+++ src/com/example/extremenotepad/Note.java
@@ -0,0 +1,27 @@
+package com.example.extremenotepad;
+
+public class Note {
+	
+	String title;
+	String note;
+	
+	public Note( String title, String note ) {
+		
+		this.title = title;
+		this.note = note;
+		
+	}
+	
+	public void setTitle( String title ) {
+		
+		this.title = title;
+		
+	}
+	
+	public void setNote( String note ) {
+		
+		this.note = note;
+		
+	}
+
+}
